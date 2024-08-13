8aa01f42b674fe96b39e468bf99371b0eb197e9c
suman <sumansapkota777@gmail.com>
2024-08-13 23:02:07 +0545

    contactRoute fixed

--- routes/contact/contactRoute.js
+++ routes/contact/contactRoute.js
@@ -1,14 +1,5 @@
 const express = require("express");
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> 1173080 (feat:sending mail)
-=======
-
-=======
->>>>>>> origin/main
->>>>>>> bfb9df58d8828ce9e2a704069d4bbdf337e9eda4
 const isAuthenticated = require("../../middleware/isAuthenticated");
 const contactForm = require("../../controller/admin/contact/contactForm");
 
